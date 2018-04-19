6b643d85f0b963832e8857f3b63b242583afdac0
wing <729411964@qq.com>
2018-04-19 21:57:33 +0800

    搭建目录
    搭建目录

---
+++ examples/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        components: {},
+        data() {
+            return {}
+        },
+        props: {},
+        computed: {},
+        methods: {},
+        watch: {},
+        created() {
+
+        }
+    }
+
+</script>
+<style scoped lang="less">
+</style>
